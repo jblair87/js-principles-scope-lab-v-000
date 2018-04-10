35e68ee0bbd1a8f2270beac090bbfeadf886774a
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-04-10 10:53:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ var customerName = "bob";
 const bestCustomer = "not Bob";
 
 function upperCaseCustomerName () {
-  return customerName = customerName.toUpperCase();
+  return customerName.toUpperCase();
 }
 function setBestCustomer () {
   return bestCustomer;
+}
+function overwriteBestCustome () {
+  let bestCustomer = "maybe bob";
 }
 function overwriteBestCustome () {
   let bestCustomer = "maybe bob";
